e58732bbcd7440bc7f07b77ec90a8b247a70f12a
mateus-coder1 <letsgood56@gmail.com>
2021-04-13 22:26:09 -0300

    Database update

--- Laboratório 2/Codigo/SQLScript/script.sql
+++ Laboratório 2/Codigo/SQLScript/script.sql
@@ -35,3 +35,11 @@ CREATE TABLE Automoveis (
 	proprietario VARCHAR(255),
 	PRIMARY KEY(matricula)
 );
+
+INSERT INTO Cliente (cpf, rg, nome, profissao, empregador, rendimento) VALUES ('12364124123', 'MG321791', 'Flávia', 'Médica', 'Hospital Madre Tereza', 12000);
+
+INSERT INTO Automoveis (matricula, ano, marca, placa, status, proprietario) VALUES ('2d2d4c4d8f', 2022, 'Volkswagen', 'AJOB123', 'NULL', 'Leonardo');
+
+INSERT INTO Endereco (cep, rua, estado, pais, cpf) VALUES ('33935389', 'rua dez', 'MG', 'Brasil', '12364124123');
+
+INSERT INTO Pedidos (codigo, status, data_inicial, data_modificacao) VALUES (3, 'Cancelado', datetime('2016-08-30 18:47:56.235'), datetime('2016-09-30 18:47:56.235') );
