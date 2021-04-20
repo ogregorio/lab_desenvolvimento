a5b5132aa6870b9be9862c749ff8abb57961c140
mateus-coder1 <letsgood56@gmail.com>
2021-04-20 10:31:30 -0300

    Update database and sql script

--- Laboratório 2/Codigo/SQLScript/script.sql
+++ Laboratório 2/Codigo/SQLScript/script.sql
@@ -21,8 +21,14 @@ CREATE TABLE Endereco (
 CREATE TABLE Pedidos (
 	codigo int AUTO_INCREMENT NOT NULL,
 	status VARCHAR(20),
-	data_inicial DATETIME,
-	data_modificacao DATETIME,
+	cpfCliente VARCHAR(11) NOT NULL,
+	matriculaAutomovel VARCHAR(10) NOT NULL,
+	codigoAgente VARCHAR(10) NOT NULL,
+	dataInicial DATETIME,
+	dataModificacao DATETIME,
+	FOREIGN KEY (cpfCliente) REFERENCES Cliente(cpf),
+	FOREIGN KEY (matriculaAutomovel) REFERENCES Automoveis(matricula),
+	FOREIGN KEY (codigoAgente) REFERENCES Agente(codigoAgente),
 	PRIMARY KEY(codigo)
 );
 
@@ -30,18 +36,24 @@ CREATE TABLE Automoveis (
 	matricula VARCHAR(10) NOT NULL,
 	ano YEAR NOT NULL,
 	marca VARCHAR(50) NOT NULL,
+	modelo VARCHAR(50) NOT NULL,
 	placa VARCHAR(7) NOT NULL,
 	status VARCHAR(20),
 	proprietario VARCHAR(255),
 	PRIMARY KEY(matricula)
 );
 
-INSERT INTO Cliente (cpf, rg, nome, profissao, empregador, rendimento) VALUES ('12364124126', 'MG421791', 'Bruna', 'Otorrinolaringologista', 'Napa center', 3000);
+CREATE TABLE Agente (
+	codigoAgente VARCHAR(10) NOT NULL,
+	PRIMARY KEY(codigoAgente)
+);
 
-INSERT INTO Automoveis (matricula, ano, marca, placa, status, proprietario) VALUES ('2d2d4c4d8f', 2022, 'Volkswagen', 'AJOB123', 'NULL', 'Leonardo');
+INSERT INTO Cliente (cpf, rg, nome, profissao, empregador, rendimento) VALUES ('12364124111', 'MG421791', 'Zico', 'Jogador', 'Flamengo', 30000);
 
-INSERT INTO Endereco (cep, rua, estado, pais, cpf) VALUES ('33935389', 'rua dez', 'MG', 'Brasil', '12364124123');
+INSERT INTO Automoveis (matricula, ano, marca, modelo, placa, status, proprietario) VALUES ('2d2d4c4d8f', 2022, 'Volkswagen', 'GOL', 'AJOB123', 'NULL', 'Leonardo');
 
-INSERT INTO Pedidos (codigo, status, data_inicial, data_modificacao) VALUES (3, 'Cancelado', datetime('2016-08-30 18:47:56.235'), datetime('2016-09-30 18:47:56.235') );
+INSERT INTO Endereco (cep, rua, estado, pais, cpf) VALUES ('33935389', 'rua dez', 'MG', 'Brasil', '12364124111');
 
-INSERT INTO Pedidos (codigo, status, data_inicial, data_modificacao) VALUES (9, 'Deferido', datetime('2021-04-15 20:42:51.235'), datetime('2021-04-18 21:47:56.235') );
+INSERT INTO Pedidos (codigo, status, cpfCliente, matriculaAutomovel, codigoAgente, dataInicial, dataModificacao) VALUES (3, 'Cancelado', '12364124111', '2d2d4c4d8f', '1234567891', datetime('2016-08-30 18:47:56.235'), datetime('2016-09-30 18:47:56.235') );
+
+INSERT INTO Agente (codigoAgente) VALUES ('1234567891');
